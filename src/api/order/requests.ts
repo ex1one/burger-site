b9c7ba17f3f03cf76df49300af6c11adeebae0f8
ilya_rogachev <ilarogacev@ilya-rogachev.local>
2025-06-10 20:22:26 +0500

    Добавил Authorization заголовок для создания заказа

--- src/api/order/requests.ts
+++ src/api/order/requests.ts
@@ -1,14 +1,22 @@
+import { YandexApi } from "../config/urls";
+import { getItemFromLocalStorage } from "../utils";
 
-import { YandexApi } from '../config/urls';
+import { Order } from "./types";
 
-import { Order } from './types';
-
-import myFetch from '@src/api/my-fetch';
+import myFetch from "@src/api/my-fetch";
 
 const BASE_URL = YandexApi;
 
 export const createOrder = (ingredientsIds: string[]) => {
-	return myFetch.post<{ success: boolean; order: Order; name: string }>(BASE_URL + '/orders', {
-		data: { ingredients: ingredientsIds },
-	});
+  const accessToken = getItemFromLocalStorage("accessToken");
+
+  return myFetch.post<{ success: boolean; order: Order; name: string }>(
+    BASE_URL + "/orders",
+    {
+      data: { ingredients: ingredientsIds },
+      headers: {
+        Authorization: accessToken,
+      },
+    }
+  );
 };
--- src/services/order/slice.ts
+++ src/services/order/slice.ts
@@ -1,32 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-import { selectors } from './selectors';
-import { actions } from './actions';
-import { extraReducers } from './extraReducers';
-import { thunks } from './thunks';
+import { selectors } from "./selectors";
+import { actions } from "./actions";
+import { extraReducers } from "./extraReducers";
+import { thunks } from "./thunks";
+
+import { APIError } from "@src/api/types/errors";
 
 export interface TInitialState {
-	status: string;
-	order: { number: number; name: string } | null;
-	isLoading: boolean;
-	error: any;
+  status: string;
+  order: { number: number; name: string } | null;
+  isLoading: boolean;
+  error: APIError | null;
 }
 
 export const initialState: TInitialState = {
-	status: 'pending',
-	order: null,
-	isLoading: false,
-	error: null,
+  status: "pending",
+  order: null,
+  isLoading: false,
+  error: null,
 };
 
 const orderSlice = createSlice({
-	name: 'order',
-	initialState,
-	reducers: actions,
-	extraReducers,
-	selectors,
+  name: "order",
+  initialState,
+  reducers: actions,
+  extraReducers,
+  selectors,
 });
 
-export const { reducer: orderReducer, actions: orderActions, selectors: orderSelectors } = orderSlice;
+export const {
+  reducer: orderReducer,
+  actions: orderActions,
+  selectors: orderSelectors,
+} = orderSlice;
 
 export const orderThunks = thunks;
